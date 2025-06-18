d3f667eb6b87c35547ca8ef30d5dd10e4f7a5178
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-06-18 14:13:57 +0300

    Add summoner names and tagLines for asia and americas

--- Model/summoners.py
+++ Model/summoners.py
@@ -1,5 +1,5 @@
 summoners = [
-     {
+    {
         "summoner_names": ["FNC Mikyx", "Nio", "fear dagger", "Sayn CHOKBAR 1", "mixed emotions", "G4KG05", "Kenal", "MIDKING", "BW CINGENEM", "Decay0"],
         "tagLines": ["1998", "Nii", "4810", "EUW", "0141", "0023", "EUW", "kkkk", "KINGO", "EUW"],
         "tier": "CHALLENGER",
@@ -59,4 +59,124 @@ summoners = [
         "tier": "IRON",
         "region": "europe"
     }, 
+    {
+        "summoner_names": ["kisno", "Sushee", "Clareetz", "Alonso", "Icy", "MageBot", "aadam", "Ego", "REDEMPTION", "SALVA"],
+        "tagLines": ["NA1", "NA1", "0002", "NA2", "0499", "SCALE", "5IFT1", "1048", "001", "1031"],
+        "tier": "CHALLENGER",
+        "region": "americas"
+    },  
+    {
+        "summoner_names": [ "Tomo", "Rabbithasu", "garbocan", "PoopSwag69", "Foreverglow", "JWARRIOR1", "Barbeau", "pepi", "heracrossguy123"],
+        "tagLines": [ "0999", "NA1", "ivern", "l33t", "castl", "Thor", "NA1", "supp", "chill"],
+        "tier": "GRANDMASTER",
+        "region": "americas"
+    }, 
+    {
+        "summoner_names": [ "FootsAndFeet", "Saneryus", "Maronite", "KeinerWeiner", "Piercing Wail"],
+        "tagLines": [ "Foots", "100", "NA1", "NA3", "NA1"],
+        "tier": "MASTER",
+        "region": "americas"
+    }, 
+    {
+        "summoner_names": [ "Smolder Era", "DukeBradley", "Jake", "almightyone4life", "Virsep", "Samira", "The Last Nap", "Rejieu", "Osmar"],
+        "tagLines": [ "225", "Duke", "wintr", "Dman", "5801", "fini", "NA1", "yamom", "NA1"],
+        "tier": "DIAMOND",
+        "region": "americas"
+    }, 
+    {
+        "summoner_names": ["rndom2350189", "Fairchild", "LpCheizer", "logiee000", "Oxn", "Michel556", "jake", "HighKa", "She so greedy", "Ninelie"],
+        "tagLines": ["NA1", "NA1", "Exile", "BOT", "NA1", "NA1", "skol", "NA1", "NA1", "StarG"],
+        "tier": "EMERALD",
+        "region": "americas"
+    }, 
+    {
+        "summoner_names": ["OmniKaizen", "Kouseo", "bablin", "BotDuosSuck", "OmniKaizen", "Pichu", "milxxx", "tpumps", "JJKING", "NOCS4U"],
+        "tagLines": ["NA1", "NA1", "NA1", "omfg", "NA1", "Hedge", "NA1", "boba", "1688", "NOJPN"],
+        "tier": "PLATINUM",
+        "region": "americas"
+    }, 
+    {
+        "summoner_names": [ "HGB", "Roomee", "Sun", "FNC tibear", "YassQween06", "Jamba Josh"],
+        "tagLines": [ "NA1", "NA1", "Show", "NA1", "NA1", "NA1"],
+        "tier": "GOLD",
+        "region": "americas"
+    }, 
+    {
+        "summoner_names": ["Goddaro", "trumpforpresiden", "A Flo480", "TUGG", "Monster129401342", "big bro", "Salt King 76", "KaiMinChun", "Bmoose", "Fìshie"],
+        "tagLines": ["NA1", "maga", "NA1", "NA1", "3899", "lols", "MJIN", "134", "moose", "NA1"],
+        "tier": "SILVER",
+        "region": "americas"
+    }, 
+    {
+        "summoner_names": ["unishaicle", "Warranty", "Mcnuggetss", "Akkha", "KellyLe1227", "ChelsOnVenus", "ACONJA", "MuichiroMisty", "Mady", "Toby"],
+        "tagLines": ["NA1", "1234", "NA1", "Floka","NA1", "NA2", "NA1", "Corps", "HER", "3119"],
+        "tier": "BRONZE",
+        "region": "americas"
+    }, 
+    {
+        "summoner_names": ["Reservoir Dog", "xlsobhanlx", "JuanMorewave", "Potat", "RealHalozeus", "hottyminustheH", "SuckerPunch21", "Silentprosksl"],
+        "tagLines": [ "NA1", "xxx", "MUTE", "6495", "NA1", "6969", "NA1", "NA1"],
+        "tier": "IRON",
+        "region": "americas"
+    }, 
+    {
+        "summoner_names": ["재훈이의모함", "livinli", "T1 Gumayusi", "DRX kyeahoo", "Serenade", "日益澄明", "qazwsx", "DK Siwoo", "Trendsetter", "Midir"],
+        "tagLines": ["OMG", "KR1", "KR1", "0813", "kr11", "TES", "abc", "DKtop", "CPHM", "DNF"],
+        "tier": "CHALLENGER",
+        "region": "asia"
+    },  
+    {
+        "summoner_names": ["Never ez", "TL CoreJJ", "청포도육포", "qwercvx", "carprain", "woshizhuyijing", "현랑 호로", "LazyFeel", "thichnuocdua", "我是野鸡"],
+        "tagLines": ["KR1", "Honda", "KR22", "KR0", "0710", "2006", "요이츠", "DRX1", "1805", "9090"],
+        "tier": "GRANDMASTER",
+        "region": "asia"
+    },  
+    {
+        "summoner_names": ["Pure Imagination", "Love song", "yizong777", "cloud", "Formuler", "dayMid", "love", "Fubuki", "MaRin", "ezl"],
+        "tagLines": ["sleep", "2011", "KR11", "beryl", "0112", "KR1", "sup", "Y01", "Pro", "1808"],
+        "tier": "MASTER",
+        "region": "asia"
+    },  
+    {
+        "summoner_names": ["your era", "역천괴", "Lets go win", "luonaerduo", "xxgg", "bzzaa", "Mabatakii", "오늘의일기", "최의현", "햄말갱"],
+        "tagLines": ["3031", "7083", "0927", "8307", "zypp", "2025", "vdy", "1234", "주먹왕랄프", "햄부기"],
+        "tier": "DIAMOND",
+        "region": "asia"
+    },  
+    {
+        "summoner_names": ["Yuki", "Jojo", "김태진님", "XRinaX", "봇캐리해볼게요", "견문색패기", "과거로돌아간다면", "근태님", "강우진님", "갱간갱수"],
+        "tagLines": ["kr97", "5766", "KR1", "KR1", "KR1", "KR1", "KR1", "KR1", "KR1", "KR1"],
+        "tier": "EMERALD",
+        "region": "asia"
+    },  
+    {
+        "summoner_names": ["The Momentary", "남양주관리인", "오늘집에못들어가", "밥좋아", "광안동쭈니", "수돌이와 건우", "유공압제어", "미끄덩", "粉红公主", "고 죠"],
+        "tagLines": ["KR1", "KR1", "KR1", "KR1", "KR1", "KR1", "KR1", "미끌미끌미", "666", "0102"],
+        "tier": "PLATINUM",
+        "region": "asia"
+    },  
+    {
+        "summoner_names": ["이재명의 펜타킬", "융프로이트아들러", "하루 십분 뇌새김", "들짐승", "dfw2", "딸요스", "나부터챙기자", "다차단하는계정", "다니입니다", "T압차이"],
+        "tagLines": ["KR1", "111", "KR1", "KR12", "KR1", "KR2", "KR1", "123", "KR1", "KR1"],
+        "tier": "GOLD",
+        "region": "asia"
+    },  
+    {
+        "summoner_names": ["No more duick", "0블룽블룽0", "ANDREWSIM", "도리도", "귀찮게하지마", "홀li데e", "넌 내려가라", "사람없다", "드론개박살호소민원인", "하얀창문"],
+        "tagLines": ["KR1", "KR1", "KRsim", "KR11", "1125", "KR1", "1190", "KR01", "KR2", "3446"],
+        "tier": "SILVER",
+        "region": "asia"
+    },  
+    {
+        "summoner_names": ["싸응민", "밀빱악끄꾸스", "스타올드맨", "잉둔곤듀", "푸른하늘흐릿한기억", "서폿마보", "트랄랄레로 트랄랄라", "크리스범스테드", "김천잔망루피", "보원장"],
+        "tagLines": ["KR1", "KR1", "KR1", "KR1", "0001", "2864", "0605", "1029", "KR1", "KR1"],
+        "tier": "BRONZE",
+        "region": "asia"
+    },  
+    {
+        "summoner_names": [ "잼민이급식철거반장", "hyeon0418", "빤짝빤짝이","달걀선생", "oo뚝빼기oo","원할머니보고쌈1557", "시비금지", "뽀앙크크", "똥오줌갤러리"],
+        "tagLines": [ "1546", "7849", "KR1", "6845", "KR1", "1557", "4266", "KR1", "문재이니"],
+        "tier": "IRON",
+        "region": "asia"
+    },  
 ] 
